233020cbfd3a47103a94d9723ea2e7da5cd19abc
labredes <labredes@pucrs.com>
2015-04-14 20:46:55 -0300

    Implemented Ethernet field

--- main.c
+++ main.c
@@ -32,27 +32,20 @@
   int on;
   struct ifreq ifr;
 
+  struct ether_header header;
 
-typedef struct  {
-	uint8_t mac_destino[6];
-	uint8_t mac_origem[6];
-	uint8_t type[2];
-}ethernet_header;
 
-ethernet_header header;
-
-void fillMacOrigem(unsigned char *buff1){
+void printEthernet(struct ether_header *header){
 	int i;
-	for(i = 0; i <= 11 ; i++){
-		header.mac_destino[i] = buff1[i];
+	printf("\nMAC Destino: ");
+	for(i = 0; i <= 5 ; i++){
+		printf("%02x " , (*header).ether_dhost[i]);
 	}
-}
-
-void fillMacDestino(unsigned char *buff1){
-	int i;
-	for(i = 6; i <= 11 ; i++){
-		header.mac_destino[i] = buff1[i];
+	printf("\nMAC Origem: ");
+	for(i = 0; i <= 5 ; i++){
+		printf("%02x " , (*header).ether_shost[i]);
 	}
+	printf("\nTipo: %04x\n", (*header).ether_type);
 }
 
 int main(int argc,char *argv[])
@@ -75,15 +68,8 @@ int main(int argc,char *argv[])
 
 	// recepcao de pacotes
 	while (1) {
-   		recv(sockd,(char *) &buff1, sizeof(buff1), 0x0);
-		ethernet_header current;
-		fillMacOrigem(buff1);
-		fillMacDestino(buff1);
-		// impress�o do conteudo - exemplo Endereco Destino e Endereco Origem
-		printf("MAC Destino: %02x:%02x:%02x:%02x:%02x:%02x \n", header.mac_destino[0],header.mac_destino[1],header.mac_destino[2],
-									header.mac_destino[3],header.mac_destino[4],header.mac_destino[5]);
-		printf("MAC Origem: %02x:%02x:%02x:%02x:%02x:%02x \n", header.mac_destino[6],header.mac_destino[7],header.mac_destino[8],
-									header.mac_destino[9],header.mac_destino[10],header.mac_destino[11]);
-		printf("Type : %02x  %02x\n\n" ,buff1[12], buff1[13] );
+		struct ether_header current;
+   		recv(sockd,&current, sizeof(current), 0x0);
+		printEthernet(&current);
 	}
 }
